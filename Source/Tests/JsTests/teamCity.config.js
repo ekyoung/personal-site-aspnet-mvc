29f7a3f93fe3f0c9161e984dbda8f580d74430dd
Ethan Young <ethankyoung@gmail.com>
2015-07-30 22:27:18 -0600

    Start trying to get the build to work.

---
+++ Source/Tests/JsTests/teamCity.config.js
@@ -0,0 +1,74 @@
+// Karma configuration
+// Generated on Thu Nov 07 2013 22:03:46 GMT-0700 (Mountain Standard Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'angular-1.2.0/angular.js',
+      'angular-1.2.0/angular-*.js',
+      '../../Web/**/*.js',
+      '**/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      'angular-1.2.0/angular-loader.js',
+      'angular-1.2.0/*.min.js',
+      'angular-1.2.0/angular-scenario.js',
+      '../../Web/Content/bootstrap-*/**/*.js'
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['teamcity'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
+  });
+};
